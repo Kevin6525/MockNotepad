21954c6b36cfa2cab489117174b105c3534d6534
kevin6525 <kevin6523@gmail.com>
2024-01-21 01:44:01 -0800

    Added more mock menu options

--- windows/main_window.py
+++ windows/main_window.py
@@ -10,9 +10,22 @@ class MainWindow(QMainWindow):
         menu_bar = self.menuBar()
         # This line is only required for Mac OS and not Windows
         menu_bar.setNativeMenuBar(False) 
-        file_menu = menu_bar.addMenu("&File")
+
+        # File menu with quit action
+        file_menu = menu_bar.addMenu("File")
         quit_action = file_menu.addAction("Quit")
         quit_action.triggered.connect(self.quit_app)
+        # Edit menu with additional actions
+        edit_menu = menu_bar.addMenu("Edit")
+        edit_menu.addAction("Copy")
+        edit_menu.addAction("Cut")
+        edit_menu.addAction("Paste")
+        edit_menu.addAction("Undo")
+        edit_menu.addAction("Redo")
+
+        window_menu = menu_bar.addMenu("Window")
+        settings_menu = menu_bar.addMenu("Settings")
+        help_menu = menu_bar.addMenu("Help")
 
     def quit_app(self):
         self.app.quit()
