c786104c5b87789e1a819400a52461846d209723
kevin6525 <kevin6523@gmail.com>
2024-01-21 01:38:57 -0800

    Added menu and quit action

--- windows/main_window.py
+++ windows/main_window.py
@@ -5,3 +5,14 @@ class MainWindow(QMainWindow):
         super().__init__()
         self.app = app
         self.setWindowTitle("Kevin's Notepad")
+
+        # Menu options/actions for our app
+        menu_bar = self.menuBar()
+        # This line is only required for Mac OS and not Windows
+        menu_bar.setNativeMenuBar(False) 
+        file_menu = menu_bar.addMenu("&File")
+        quit_action = file_menu.addAction("Quit")
+        quit_action.triggered.connect(self.quit_app)
+
+    def quit_app(self):
+        self.app.quit()
